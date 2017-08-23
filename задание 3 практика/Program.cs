065b23da5f273421edc6213d9793f1c79125e783
user <user@LAPTOP-M9GFNJT2>
2017-08-23 10:35:35 +0500

    Добавьте файлы проекта.

---
+++ задание 3 практика/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_3_практика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            int x;
+            bool ok = false;
+
+            do
+            {
+                Console.WriteLine("Введите х");
+                string user = Console.ReadLine();
+                ok = int.TryParse(user, out x);
+                if (!ok) Console.WriteLine("Неверный ввод");
+            }
+            while (!ok);
+
+            if (x > 0) y = -x * x;
+            else y = -x;
+
+            Console.WriteLine("y=" + y);
+            Console.ReadKey();
+
+        }
+    }
+}
